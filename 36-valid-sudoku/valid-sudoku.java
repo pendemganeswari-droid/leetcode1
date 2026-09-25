dd480e79110b747de783169c6e8631ab39351c63
pendemganeswari-droid <pendemganeswari@gmail.com>
2026-09-25 18:38:13 +0530

    Time: 1 ms (99.93%) | Memory: 46.7 MB (27.65%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,60 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+
+        for (int i = 0; i < 9; i++) {
+            boolean[] seen = new boolean[9];
+
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+
+                    if (seen[num]) {
+                        return false;
+                    }
+
+                    seen[num] = true;
+                }
+            }
+        }
+
+        for (int j = 0; j < 9; j++) {
+            boolean[] seen = new boolean[9];
+
+            for (int i = 0; i < 9; i++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+
+                    if (seen[num]) {
+                        return false;
+                    }
+
+                    seen[num] = true;
+                }
+            }
+        }
+
+        for (int row = 0; row < 9; row += 3) {
+            for (int col = 0; col < 9; col += 3) {
+
+                boolean[] seen = new boolean[9];
+
+                for (int i = row; i < row + 3; i++) {
+                    for (int j = col; j < col + 3; j++) {
+
+                        if (board[i][j] != '.') {
+                            int num = board[i][j] - '1';
+
+                            if (seen[num]) {
+                                return false;
+                            }
+
+                            seen[num] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
